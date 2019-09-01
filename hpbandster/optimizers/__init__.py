9933a2e14dc4ab04ff98b46611e4e04cd29000c2
cherubim <cherubim@cherubim.fritz.box>
2019-09-01 10:43:43 -0400

    Added BOHB_Multi_KDE

--- hpbandster/optimizers/__init__.py
+++ hpbandster/optimizers/__init__.py
@@ -1,4 +1,5 @@
 from hpbandster.optimizers.randomsearch import RandomSearch
 from hpbandster.optimizers.hyperband import HyperBand
 from hpbandster.optimizers.bohb import BOHB
+from hpbandster.optimizers.bohb_multi_kde import BOHB_Multi_KDE
 from hpbandster.optimizers.h2bo import H2BO
